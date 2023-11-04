fd629a92f04106c43f2939a362b892fe2b39d3a0
orlandoc0107 <orlandocardenas0107@gmail.com>
2023-11-04 04:30:46 -0300

    update

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -14,6 +14,7 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
@@ -31,6 +32,7 @@ export default async function CardWrapper() {
 }
 
 export function Card({
+  
   title,
   value,
   type,
